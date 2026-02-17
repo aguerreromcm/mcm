aa4f82f36c1340f6b3474e64966e0b3b6edf1d89
soporteMCM <alberto.s@masconmenos.com.mx>
2026-02-17 17:48:20 -0600

    Permisos de ahorro para cajas

--- backend/App/controllers/Contenedor.php
+++ backend/App/controllers/Contenedor.php
@@ -29,10 +29,6 @@ class Contenedor extends Controller
         $perfil = $this->__perfil;
         $permiso_ahorro = $this->__ahorro;
 
-        $config = \Core\App::getConfig();
-        $mostrarHerramientas = (isset($config['HERRAMIENTAS_CLAVE']) && trim((string) $config['HERRAMIENTAS_CLAVE']) === 'accesoh');
-        $menu = (new \App\components\Menu($perfil, $usuario, $permiso_ahorro, $mostrarHerramientas))->mostrar();
-
         $header = <<<HTML
             <!DOCTYPE html>
             <html lang="es">
@@ -79,30 +75,36 @@ class Contenedor extends Controller
                                         <span><b>PERFIL: </b><span class="fa fa-key"></span>{$perfil}</span>
                                     </div>
                                 </div>
-                                {$menu}
-                            </div>
+        HTML;
+
+        $config = \Core\App::getConfig();
+        $mostrarHerramientas = (isset($config['HERRAMIENTAS_CLAVE']) && trim((string) $config['HERRAMIENTAS_CLAVE']) === 'accesoh');
+        $menu = (new \App\components\Menu($perfil, $usuario, $permiso_ahorro, $mostrarHerramientas))->mostrar();
+
+        $menu .= <<<HTML
+                </div>
+            </div>
+            <div class="top_nav">
+                <div class="nav_menu">
+                    <nav>
+                        <div class="nav toggle">
+                            <a id="menu_toggle"><i class="fa fa-bars"></i></a>
                         </div>
-                        <div class="top_nav">
-                            <div class="nav_menu">
-                                <nav>
-                                    <div class="nav toggle">
-                                        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-                                    </div>
-                                    <ul class="nav navbar-nav navbar-right">
-                                        <li class="">
-                                            <a href="" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                                <span class=" fa fa-user"></span> {$nombre}
-                                                <span class=" fa fa-angle-down"></span>
-                                            </a>
-                                            <ul class="dropdown-menu dropdown-usermenu pull-right">
-                                                <li><a href="/Login/cerrarSession"><i class="fa fa-sign-out pull-right"></i>Cerrar Sesión</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="">
+                                <a href="" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                    <span class=" fa fa-user"></span> {$nombre}
+                                    <span class=" fa fa-angle-down"></span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-usermenu pull-right">
+                                    <li><a href="/Login/cerrarSession"><i class="fa fa-sign-out pull-right"></i>Cerrar Sesión</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
         HTML;
 
         return $header . $menu;
@@ -111,26 +113,26 @@ class Contenedor extends Controller
     public function footer($extra = '')
     {
         $footer = <<<HTML
-                </div>
-                <script src="/js/moment/moment.min.js"></script>
-                <script src="/js/sweetalert.min.js"></script>
-                <script src="/js/jquery.min.js"></script>
-                <script src="/js/bootstrap.min.js"></script>
-                <script src="/js/bootstrap/bootstrap-switch.js"></script>
-                <script src="/js/nprogress.js"></script>
-                <script src="/js/custom.min.js"></script>
-                <script src="/js/validate/jquery.validate.js"></script>
-                <script src="/js/login.js"></script>
-                <script src="/js/tabla/jquery.dataTables.min.js"></script>
-                <script src="/js/tabla/dataTables.bootstrap.min.js"></script>
-                <script src="/js/tabla/jquery.tablesorter.js"></script>
-                <script src="/js/dataTables.buttons.min.js" ></script>
-                <script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" ></script>
-                <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js" ></script>
-                <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js" ></script>
-                <script src="//cdn.datatables.net/buttons/1.4.2/js/buttons.html5.min.js" ></script>
-                $extra
-            </body>
+            </div>
+            <script src="/js/moment/moment.min.js"></script>
+            <script src="/js/sweetalert.min.js"></script>
+            <script src="/js/jquery.min.js"></script>
+            <script src="/js/bootstrap.min.js"></script>
+            <script src="/js/bootstrap/bootstrap-switch.js"></script>
+            <script src="/js/nprogress.js"></script>
+            <script src="/js/custom.min.js"></script>
+            <script src="/js/validate/jquery.validate.js"></script>
+            <script src="/js/login.js"></script>
+            <script src="/js/tabla/jquery.dataTables.min.js"></script>
+            <script src="/js/tabla/dataTables.bootstrap.min.js"></script>
+            <script src="/js/tabla/jquery.tablesorter.js"></script>
+            <script src="/js/dataTables.buttons.min.js" ></script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" ></script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js" ></script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js" ></script>
+            <script src="//cdn.datatables.net/buttons/1.4.2/js/buttons.html5.min.js" ></script>
+            $extra
+        </body>
         </html>
         HTML;
         return $footer;
