05203cdd812b5559186fd542baaaa456de3a2b4e
MCM-Server <>
2026-04-17 12:05:44 -0600

    Ajuste de permisos para reporte de devengo

--- backend/App/components/Menu.php
+++ backend/App/components/Menu.php
@@ -154,7 +154,7 @@ class Menu
             $this->enlace('Conciliación de pagos', '/Operaciones/ConciliacionPagos/', ['AMGM']),
             $this->enlace('Cierre de día', '/Operaciones/CierreDiario/', ['AMGM']),
             $this->enlace('Reporte Cliente y Aval Consolidado', '/Operaciones/ReportePC', ['ADMIN', 'PHEE', 'MCDP', 'FECR', 'ORHM']),
-            $this->enlace('Reporte Interés Devengado', '/Operaciones/ReporteInteresDevengado', ['ADMIN', 'ORHM']),
+            $this->enlace('Reporte Interés Devengado', '/Operaciones/ReporteInteresDevengado', ['AMGM', 'ORHM']),
             $this->enlace('Reporte Acreditado', '/Operaciones/ReporteAcreditado', ['ADMIN', 'ORHM', 'LGFR', 'MCDP', 'MAPH'])
         ];
     }
